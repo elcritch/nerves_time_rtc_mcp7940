e01d4e003a1c86b3ff211bf1c19d5081e1fdfe20
Jaremy Creechley <creechley@gmail.com>
2020-07-08 13:37:12 -0600

    version bump from last release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesTime.RTC.MCP7940.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/elcritch/nerves_time_rtc_mcp7940"
 
   def project do
